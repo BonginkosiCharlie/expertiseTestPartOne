35c6fb632c2af6cae91d1aadbb60cdf79a940cd1
BonginkosiCharlie <bonginkosimtyali@gmail.com>
2020-12-11 17:44:26 +0200

    Added controller with ktoc and ctok endpoints

--- src/main/java/com/expertiseTestPartOne/Controllers/ConversionController.java
+++ src/main/java/com/expertiseTestPartOne/Controllers/ConversionController.java
@@ -1,5 +1,29 @@
 package com.expertiseTestPartOne.Controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.expertiseTestPartOne.Models.Temperature;
+
+
+@RestController
+@RequestMapping("/conversions")
 public class ConversionController {
+	
+	@PostMapping("/ktoc")
+	public String convertKelvinToCelsius(@RequestBody Temperature temp) {
+		temp.convertK2C();
+		System.out.println(temp);
+		return temp.toString();
+	}
+	
+	@PostMapping("/ctok")
+	public String convertCelsiusToKelvin(@RequestBody Temperature temp) {
+		temp.convertC2K();
+		System.out.println(temp);
+		return temp.toString();
+	}
 
 }

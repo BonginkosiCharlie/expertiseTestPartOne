c12912891dc0b93bcf30340efd2faf82ef90ab39
BonginkosiCharlie <bonginkosimtyali@gmail.com>
2020-12-11 17:15:13 +0200

    Springboot initializer

---
+++ src/main/java/com/expertiseTestPartOne/expertiseTestConversions/ExpertiseTestConversionsApplication.java
@@ -0,0 +1,13 @@
+package com.expertiseTestPartOne.expertiseTestConversions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertiseTestConversionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertiseTestConversionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expertiseTestPartOne/expertiseTestConversions/ExpertiseTestConversionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expertiseTestPartOne.expertiseTestConversions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertiseTestConversionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
